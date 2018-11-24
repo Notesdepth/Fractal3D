57ea9585d42dfe986e59bb1ac35f75268ca3931e
Notesdepth <cedric.tesse@laposte.net>
2018-11-24 23:43:46 +0100

    first again

---
+++ C/Oscillateur/src/init.c
@@ -0,0 +1,5 @@
+//
+// Created by Notesdepth on 24/11/2018.
+//
+
+#include "oscillateur.h"
---
+++ C/Oscillateur/src/main.c
@@ -0,0 +1,10 @@
+//
+// Created by Notesdepth on 24/11/2018.
+//
+
+#include "oscillateur.h"
+int main()
+{
+    printf("coucou\n");
+    return (0);
+}
